7aa8f6b26a848ec4131cecc40d248e5789b13189
Bello Iyemifokhae <iyemifokhaebello@gmail.com>
2022-12-12 12:23:34 -0800

    Added a digit number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+/**
+ * main - Print all single digit
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0, i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	purchar('\n');
+	return (0);
+}

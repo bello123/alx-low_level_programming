d73b36fa9679ed5352238c072b1b37c42862e117
Bello Iyemifokhae <iyemifokhaebello@gmail.com>
2022-12-15 09:03:59 -0800

    Added a fi;e

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - Print a file
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
